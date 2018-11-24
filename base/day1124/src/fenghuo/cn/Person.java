6e071904a5a44f72187bf3aaf1451ffdf3ca1d9e
wjm <wjm@qq.com>
2018-11-24 18:16:26 +0800

    把本地工程添加到Git3

---
+++ base/day1124/src/fenghuo/cn/Person.java
@@ -0,0 +1,28 @@
+package fenghuo.cn;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(){};
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
