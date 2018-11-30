4e4d04dc7ea114d27d8487f07395552f1ab9f305
wjm <wjm@qq.com>
2018-12-01 00:24:50 +0800

    添加换行快捷注释!!!! 提交11.30

--- base/day1124/src/fenghuo/cn/beijing.java
+++ base/day1124/src/fenghuo/cn/beijing.java
@@ -1,6 +1,8 @@
 package fenghuo.cn;
 
 
+import java.util.ArrayList;
+
 public class beijing {
     public static void main(String[] args) {
         System.out.println("hello git project");
@@ -9,6 +11,15 @@ public class beijing {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        System.out.println("hello git project");
+        ArrayList<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");//ctrl+shift+enter 自动补全分号结束语句!!!!!shift+enter跳出括号另起一行!!!!
+        for (String st :
+                list) {
+            System.out.println(st);
+        }
         //hello test
     }
 
